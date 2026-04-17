7f258f9a38c984789ee9660748c6efbfa9462782
GuptaYukti <164582524+GuptaYukti@users.noreply.github.com>
2026-04-17 15:32:58 -0400

    Mask DB_PASSWORD in database connection setup
    Updated database.py to mask DB_PASSWORD in the environment variable.

--- pythonsql/database.py
+++ pythonsql/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DB_USER = os.getenv("DB_USER",'root')
-DB_PASSWORD = urllib.parse.quote_plus(os.getenv("DB_PASSWORD", "Incedo@123"))
+DB_PASSWORD = urllib.parse.quote_plus(os.getenv("DB_PASSWORD", "xxx"))
 
 DB_HOST = os.getenv("DB_HOST",'localhost')
 DB_NAME = os.getenv("DB_NAME",'mydatabase')
